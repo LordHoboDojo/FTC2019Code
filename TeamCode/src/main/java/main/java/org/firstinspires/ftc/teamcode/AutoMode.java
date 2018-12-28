c8b3e36e1374c19cfb0dcfaef1777294b8c3d8a1
LordHoboDojo <32212717+LordHoboDojo@users.noreply.github.com>
2018-12-28 14:21:37 -0600

    Auto Update

--- TeamCode/src/main/java/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
+++ TeamCode/src/main/java/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
@@ -7,33 +7,52 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name = "The Auto Mode")
 public class AutoMode extends LinearOpMode {
     private DcMotor upDrive = null;
-   private ElapsedTime runtime = new ElapsedTime();
-   private DcMotor leftDrive= null;
-   private DcMotor rightDrive = null;
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive= null;
+    private DcMotor rightDrive = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
         waitForStart();
-    upDrive = hardwareMap.get(DcMotor.class, "up_drive");
-    leftDrive = hardwareMap.get(DcMotor.class,"left_drive");
-    rightDrive = hardwareMap.get(DcMotor.class,"right_drive");
-    leftDrive.setDirection(DcMotor.Direction.FORWARD);
-    rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        upDrive = hardwareMap.get(DcMotor.class, "up_drive");
+        leftDrive = hardwareMap.get(DcMotor.class,"left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class,"right_drive");
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
-    upDrive.setPower(-.5);
-    runtime.reset();
-    while(opModeIsActive()&& runtime.seconds()<3)
-    {
-        //Party
+        upDrive.setPower(-.5);
+        runtime.reset();
+        while(opModeIsActive()&& runtime.seconds()<2.7)
+        {
+            //Party
+        }
+        upDrive.setPower(0);
+        leftDrive.setPower(.5);
+        rightDrive.setPower(.5);
+        runtime.reset();
+        while (opModeIsActive() && runtime.seconds()<.1)
+        {
+            //party
+        }
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+        upDrive.setPower(.5);
+        runtime.reset();
+        while(opModeIsActive()&& runtime.seconds()<2.7)
+        {
+            //Party
+        }
+        upDrive.setPower(0);
+
+        leftDrive.setPower(.5);
+        rightDrive.setPower(-.5);
+        while (opModeIsActive() && runtime.seconds()<.25)
+        {
+            //party
+        }
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
     }
 
-    leftDrive.setPower(-3);
-    rightDrive.setPower(-3);
-    runtime.reset();
-    while (opModeIsActive() && runtime.seconds()<5)
-    {
-        //party
-    }
-
-    }
 }
