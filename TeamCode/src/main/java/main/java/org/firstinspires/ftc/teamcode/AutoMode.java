35033f7ae90b3e5ffc5162451f4ce6ed865d6ac8
LordHoboDojo <32212717+LordHoboDojo@users.noreply.github.com>
2018-12-27 18:35:07 -0600

    First Auto base program

--- TeamCode/src/main/java/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
+++ TeamCode/src/main/java/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
@@ -6,22 +6,34 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name = "The Auto Mode")
 public class AutoMode extends LinearOpMode {
-    DcMotor upDrive = null;
-    ElapsedTime runtime = new ElapsedTime();
+    private DcMotor upDrive = null;
+   private ElapsedTime runtime = new ElapsedTime();
+   private DcMotor leftDrive= null;
+   private DcMotor rightDrive = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
         waitForStart();
     upDrive = hardwareMap.get(DcMotor.class, "up_drive");
+    leftDrive = hardwareMap.get(DcMotor.class,"left_drive");
+    rightDrive = hardwareMap.get(DcMotor.class,"right_drive");
+    leftDrive.setDirection(DcMotor.Direction.FORWARD);
+    rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
-    upDrive.setPower(.5);
+    upDrive.setPower(-.5);
     runtime.reset();
-    while(opModeIsActive()&& runtime.seconds()<2)
+    while(opModeIsActive()&& runtime.seconds()<3)
     {
         //Party
     }
-    upDrive.setPower(0);
 
+    leftDrive.setPower(-.5);
+    rightDrive.setPower(-.5);
+    runtime.reset();
+    while (opModeIsActive() && runtime.seconds()<2.5)
+    {
+        //party
+    }
 
     }
 }
