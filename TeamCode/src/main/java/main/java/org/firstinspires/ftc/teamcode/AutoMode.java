9a2683ab2fcaee051c2849fe2a5c56975ed8aca5
tejesus66 <42523406+tejesus66@users.noreply.github.com>
2019-01-06 10:20:48 -0600

    Auto. Post vacation update

--- TeamCode/src/main/java/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
+++ TeamCode/src/main/java/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
@@ -1,8 +1,14 @@
 package main.java.org.firstinspires.ftc.teamcode;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name = "The Auto Mode")
 public class AutoMode extends LinearOpMode {
@@ -10,49 +16,142 @@ public class AutoMode extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive= null;
     private DcMotor rightDrive = null;
-
+   //private Servo servo = null;
+    ColorSensor colorSensorLeft;    // Hardware Device Object
+    ColorSensor colorSensorRight;
     @Override
     public void runOpMode() throws InterruptedException {
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
         waitForStart();
+        //Init of robot maps
         upDrive = hardwareMap.get(DcMotor.class, "up_drive");
         leftDrive = hardwareMap.get(DcMotor.class,"left_drive");
         rightDrive = hardwareMap.get(DcMotor.class,"right_drive");
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
-        upDrive.setPower(-.5);
+        // get a reference to our ColorSensor object.
+        colorSensorLeft = hardwareMap.get(ColorSensor.class, "sensor_color_left");
+        colorSensorRight = hardwareMap.get(ColorSensor.class, "sensor_color_right");
+        colorSensorLeft.enableLed(true);
+        colorSensorRight.enableLed(true);
+        //Arm extension
         runtime.reset();
-        while(opModeIsActive()&& runtime.seconds()<2.7)
+        upDrive.setPower(-.35);
+        while(opModeIsActive()&& runtime.seconds()<1)
         {
             //Party
         }
+        runtime.reset();
         upDrive.setPower(0);
+        while(opModeIsActive()&& runtime.seconds()<4)
+        {
+            //Party
+        }
+        runtime.reset();
+        upDrive.setPower(-.5);
+        while(opModeIsActive()&& runtime.seconds()<1.5) {
+
+        }
+        upDrive.setPower(0);
+    //backward
+        runtime.reset();
         leftDrive.setPower(.5);
         rightDrive.setPower(.5);
-        runtime.reset();
-        while (opModeIsActive() && runtime.seconds()<.1)
+        while (opModeIsActive() && runtime.seconds()<.15)
         {
             //party
         }
         leftDrive.setPower(0);
         rightDrive.setPower(0);
-
-        upDrive.setPower(.5);
+    //arm compression
         runtime.reset();
+        upDrive.setPower(.5);
         while(opModeIsActive()&& runtime.seconds()<2.7)
         {
             //Party
         }
         upDrive.setPower(0);
-
-        leftDrive.setPower(.5);
-        rightDrive.setPower(-.5);
-        while (opModeIsActive() && runtime.seconds()<.25)
+    //turn right
+        runtime.reset();
+        leftDrive.setPower(-.5);
+        rightDrive.setPower(.5);
+        while (opModeIsActive() && runtime.seconds()<0.7)
         {
             //party
         }
         leftDrive.setPower(0);
         rightDrive.setPower(0);
+    //forward
+        runtime.reset();
+        leftDrive.setPower(-.5);
+        rightDrive.setPower(-.5);
+        while (opModeIsActive() && runtime.seconds()<0.775){
+
+        }
+        runtime.reset();
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        while (opModeIsActive() && runtime.seconds()<1)
+        {
+            //party
+        }
+ /*       if (colorSensorLeft.alpha()-colorSensorRight.alpha()<=0.4*colorSensorLeft.alpha()){
+            //backwards
+            runtime.reset();
+            leftDrive.setPower(.5);
+            rightDrive.setPower(.5);
+            while (opModeIsActive() && runtime.seconds()<.175)
+            {
+                //party
+            }
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+            //turn left
+            runtime.reset();
+            leftDrive.setPower(.5);
+            rightDrive.setPower(-.5);
+            while (opModeIsActive() && runtime.seconds()<0.7)
+            {
+                //party
+            }
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+            //forwards
+            runtime.reset();
+            leftDrive.setPower(-.5);
+            rightDrive.setPower(-.5);
+            while (opModeIsActive() && runtime.seconds()<1){
+
+            }
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+            //turn right
+            runtime.reset();
+            leftDrive.setPower(-.5);
+            rightDrive.setPower(.5);
+            while (opModeIsActive() && runtime.seconds()<0.75)
+            {
+                //party
+            }
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+            //forwards
+            runtime.reset();
+            leftDrive.setPower(-.5);
+            rightDrive.setPower(-.5);
+            while (opModeIsActive() && runtime.seconds()<0.75){
+
+            }
+            leftDrive.setPower(0);
+            rightDrive.setPower(0);
+        }
+        telemetry.addData("Clear", colorSensorLeft.alpha());
+        telemetry.update(); */
     }
 
 }
--- TeamCode/src/main/java/main/java/org/firstinspires/ftc/teamcode/TelOp.java
+++ TeamCode/src/main/java/main/java/org/firstinspires/ftc/teamcode/TelOp.java
@@ -1,17 +1,23 @@
 package main.java.org.firstinspires.ftc.teamcode;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
 import com.qualcomm.hardware.lynx.LynxDcMotorController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-@TeleOp(name="The Mode", group="Linear Opmode")
-public class Test extends LinearOpMode {
+@TeleOp(name="The Tele Mode", group="Linear Opmode")
+public class TelOp extends LinearOpMode {
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
 //    private Servo testServo = null;
     private DcMotor linearActuator = null;
+    private DcMotor armDrive = null;
     @Override
     public void runOpMode() throws InterruptedException {
         boolean right = false;
@@ -20,6 +26,7 @@ public class Test extends LinearOpMode {
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
         //testServo = hardwareMap.get(Servo.class , "test_Servo");
         linearActuator = hardwareMap.get(DcMotor.class, "up_drive");
+        armDrive = hardwareMap.get(DcMotor.class, "arm_drive");
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
@@ -30,8 +37,10 @@ public class Test extends LinearOpMode {
             double leftPower;
             double rightPower;
             double upPower;
+            double armPower;
             double drive = gamepad1.left_stick_y*.5;
             double turn  =  -gamepad1.right_stick_x*.5;
+            armPower = gamepad2.right_trigger-gamepad2.left_trigger;
             upPower = gamepad1.right_trigger-gamepad1.left_trigger;
 //            right = gamepad1.a;
 //            left = gamepad1.b;
@@ -45,12 +54,15 @@ public class Test extends LinearOpMode {
 //                testServo.setPosition(testServo.getPosition()-.05);
 //                delay();
 //            }
+            armPower = Range.clip(armPower,-1.0,1.0);
             upPower = Range.clip(upPower,-1.0,1.0);
             leftPower    = Range.clip(drive+turn , -1.0, 1.0) ;
             rightPower   = Range.clip(drive-turn , -1.0, 1.0) ;
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
             linearActuator.setPower(upPower);
+            armDrive.setPower(armPower*.5);
+
         }
     }
 
